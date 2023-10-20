e6f4192a3eb3848cd05d8c4edd6c433c51a91469
Darran <darran@hunt.net.nz>
2023-10-20 21:01:21 +0000

    Update to use tiny aes api, do read and write

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # vencrypt
+
+- Build
+        make
+
+- load
+        sudo insmod vencrypt.ko encrypt=1 key=0123456789ABCDEF
--- vencrypt_main.c
+++ vencrypt_main.c
@@ -7,10 +7,6 @@
 
 #include <linux/crypto.h>
 #include <linux/scatterlist.h>
-#include <crypto/skcipher.h>
-#include <crypto/hash.h>
-#include <crypto/if_alg.h>
-#include <crypto/drbg.h>
 
 #include "aes/aes.h"
 
@@ -25,11 +21,14 @@ typedef struct vencrypt_device_data {
 	struct cdev cdev;
 	// AES support data...
 	int nodeId;
-	struct crypto_skcipher *skcipher;
+	struct AES_ctx ctx;
 	unsigned char iv[16];
 	unsigned char *output_data;
 	size_t output_size;
+	size_t output_offset;
 	size_t input_processed;
+	struct vencrypt_device_data *pt;
+	struct vencrypt_device_data *ct;
 } vencrypt_device_data_t;
 
 static struct class *vencrypt_class;
@@ -65,38 +64,42 @@ static int vencrypt_open(struct inode *inode, struct file *fp)
 
 	// clear IV on open
 	memset(&vencrypt_data->iv[0], 0, sizeof(vencrypt_data->iv));
+	AES_init_ctx_iv(&vencrypt_data->ctx, key, vencrypt_data->iv);
 
 	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
 	return 0;
 }
 
-typedef struct {
-	struct scatterlist sg_in;
-	struct scatterlist sg_out;
-	struct crypto_skcipher *tfm;
-	struct skcipher_request *req;
-	struct crypto_wait wait;
-} skcipher_def_t;
+/**
+ * Get padding remainder size per PKCS#7
+ */
+static size_t get_remainder(size_t size, size_t block_size)
+{
+	size_t remainder = size % block_size;
+	if (remainder == 0) {
+		// Per PKCS#7 add 16 bytes to enable single byte pad to be recognised
+		remainder = block_size;
+	}
+	return remainder;
+}
 
 /**
- * perform cipher operation
+ * perform cipher operation (encrypt or decrypt)
  */
-
-#if 0
-static int do_crypt(vencrypt_device_data_t *vencrypt_data, bool encrypt,
-		    const unsigned char *key_string,
-		    const unsigned char *input_data, size_t input_size)
+static ssize_t do_crypt(vencrypt_device_data_t *vencrypt_data, bool encrypt,
+			const unsigned char *key_string,
+			const unsigned char *input_data, size_t input_size,
+			size_t input_offset)
 {
-	struct AES_ctx ctx;
 	size_t key_size = strlen(key_string) / 2;
+	size_t output_size;
 	unsigned char key[VE_MAX_KEY_SIZE];
 	int ind;
-	int remainder;
 	unsigned char *buf;
 
 	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
 	// check key is appropriately sized
-	// XXX fixed size key. 256?
+	// XXX fixed size key. 128?
 	if (((key_size * 2) != strlen(key_string)) ||
 	    (strlen(key_string) > (VE_MAX_KEY_SIZE * 2))) {
 		// bad / odd key size
@@ -124,189 +127,46 @@ static int do_crypt(vencrypt_device_data_t *vencrypt_data, bool encrypt,
 		key[ind] = (unsigned char)data;
 	}
 
-	// pad using PKCS#7 as needed
-	remainder = input_size % VE_BLOCK_SIZE;
-	if (remainder == 0) {
-		// Per PKCS#7 add 16 bytes to enable single byte pad to be recognised
-		remainder = VE_BLOCK_SIZE;
-	}
-
-	buf = kmalloc(input_size + remainder, GFP_KERNEL);
-	if (buf == NULL) {
-		pr_err("%s: Failed to allocate buffer\n", __func__);
-		return -EAGAIN;
-	}
-	memcpy(buf, input_data, input_size);
-	// pad
-	for (ind = 0; ind < remainder; ind++) {
-		// per PKCS#7 use the pad size as the pad
-		buf[input_size + ind] = remainder;
-	}
-
-	AES_init_ctx_iv(&ctx, key, vencrypt_data->iv);
-	AES_CBC_encrypt_buffer(&ctx, buf, input_size + remainder);
 	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	printk("VENCRYPT: %s completed\n", __func__);
-
-	return 0;
-}
-
-#else
-static int do_crypt(vencrypt_device_data_t *vencrypt_data,
-             bool encrypt, const unsigned char *key_string,
-			 const unsigned char *input_data, size_t input_size)
-{
-	int ret = -EFAULT;
-	int ind;
-	struct crypto_skcipher *skcipher = vencrypt_data->skcipher;
-	struct skcipher_request *req = NULL;
-	size_t key_size = strlen(key_string) / 2;
-	unsigned char key[VE_MAX_KEY_SIZE];
-	skcipher_def_t sk;
-    unsigned char buf[VE_BLOCK_SIZE];
-
-	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-
-	if (((key_size * 2) != strlen(key_string)) ||
-	    (strlen(key_string) > (VE_MAX_KEY_SIZE * 2))) {
-		// bad / odd key size
-		pr_info("bad key string length %lu\n", strlen(key_string));
-		ret = -EAGAIN;
-		goto out;
-	}
-
-	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	// convert key to binary
-	for (ind = 0; ind < key_size; ind++) {
-		char digits[3] = { key_string[ind * 2], key_string[ind * 2 + 1],
-				   0 };
-		long data;
-		int res;
-
-		res = kstrtol(digits, 16, &data);
-		if (res) {
-			// invalid character(s)
-			pr_info("bad key string content at index %d \"%s\"\n",
-				ind * 2, digits);
-			ret = -EAGAIN;
-			goto out;
+	if (encrypt) {
+		// pad using PKCS#7 as needed
+		int remainder = get_remainder(input_size, VE_BLOCK_SIZE);
+		printk("VENCRYPT: %s.%d remainder %d\n", __func__, __LINE__,
+		       remainder);
+		buf = vmalloc(input_size + remainder);
+		if (buf == NULL) {
+			pr_err("%s: Failed to allocate buffer\n", __func__);
+			return -EAGAIN;
 		}
-		key[ind] = (unsigned char)data;
-	}
-
-	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	req = skcipher_request_alloc(skcipher, GFP_KERNEL);
-	if (!req) {
-		pr_info("could not allocate skcipher request\n");
-		ret = -ENOMEM;
-		goto out;
-	}
-
-	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	skcipher_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG,
-				      crypto_req_done, &sk.wait);
-
-	printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	if (crypto_skcipher_setkey(skcipher, key, key_size)) {
-		pr_info("key could not be set for size %lu. Key size should be 16, 24, or 32.\n",
-			key_size);
-		ret = -EAGAIN;
-		goto out;
-	}
-
-	sk.tfm = skcipher;
-	sk.req = req;
-
-	/* do the encrypt / decrypt of the supplied data */
-#if 0
-	for (ind = 0; ind < input_size / VE_BLOCK_SIZE; ind++) {
-        printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-		sg_init_one(&sk.sg_in, &input_data[ind * VE_BLOCK_SIZE],
-			    VE_BLOCK_SIZE);
-		sg_init_one(&sk.sg_out, &buf, VE_BLOCK_SIZE);
-		skcipher_request_set_crypt(req, &sk.sg_in, &sk.sg_out,
-					   VE_BLOCK_SIZE, vencrypt_data->iv);
-        printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-		crypto_init_wait(&sk.wait); // synchronous
-
-		/* encrypt data */
-		if (encrypt) {
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-			ret = crypto_wait_req(crypto_skcipher_encrypt(sk.req),
-					      &sk.wait);
-			if (!ret) {
-				pr_info("%s: Block %d encrypted successfully\n",
-					__func__, ind);
-			}
-		} else {
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-			ret = crypto_wait_req(crypto_skcipher_decrypt(sk.req),
-					      &sk.wait);
-			if (!ret) {
-				pr_info("%s: Block %d decrypted successfully\n",
-					__func__, ind);
-			}
+		printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
+		copy_from_user(buf, input_data + input_offset, input_size);
+		printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
+		// pad
+		for (ind = 0; ind < remainder; ind++) {
+			// per PKCS#7 use the pad size as the pad value
+			buf[input_size + ind] = remainder;
 		}
+		printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
+		AES_CBC_encrypt_buffer(&vencrypt_data->ctx, buf,
+				       input_size + remainder);
+		vencrypt_data->ct->output_data = buf;
+		output_size = input_size + remainder;
+		vencrypt_data->ct->output_size = output_size;
+	} else {
+		int remainder;
+		buf = vmalloc(input_size);
+		copy_from_user(buf, input_data + input_offset, input_size);
+		AES_CBC_decrypt_buffer(&vencrypt_data->ctx, buf, input_size);
+		// get padding size
+		remainder = buf[input_size - 1];
+		vencrypt_data->pt->output_data = buf;
+		output_size = input_size - remainder;
+		vencrypt_data->pt->output_size = output_size;
 	}
-	// handle partial block
-	// - need to keep data incase more arrives
-	// - need to incorporate it into the next rounds
-	// - keep one partial bock in dev, use to start rounds
-#else
-    printk("VENCRYPT: %s.%d init sg_in input_size %lu\n", __func__, __LINE__, input_size);
-    sg_init_one(&sk.sg_in, input_data, input_size);
-    sg_init_one(&sk.sg_out, vencrypt_data->output_data, vencrypt_data->output_size);
-    skcipher_request_set_crypt(req, &sk.sg_in, &sk.sg_out, input_size,
-            vencrypt_data->iv);
-    crypto_init_wait(&sk.wait); // synchronous
+	printk("VENCRYPT: %s completed %lu -> %lu bytes\n", __func__,
+	       input_size, output_size);
 
-    /* encrypt data */
-    if (encrypt) {
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-#if 0
-            ret = crypto_wait_req(crypto_skcipher_encrypt(sk.req),
-                                  &sk.wait);
-#else
-            ret = 0;
-#endif
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-            if (!ret) {
-                    pr_info("%s: buffer encrypted successfully\n",
-                            __func__);
-            }
-    } else {
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-            ret = crypto_wait_req(crypto_skcipher_decrypt(sk.req),
-                                  &sk.wait);
-            printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-            if (!ret) {
-                    pr_info("%s: Buffer decrypted successfully\n",
-                            __func__);
-            }
-    }
-
-    if (!ret) {
-        printk("VENCRYPT: %s.%d success\n", __func__, __LINE__);
-    }
-#endif
-
-    //sg_copy_to_buffer(&mgc->sg, 1, ciphertext, datasize);
-
-    printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	if (!ret) {
-		pr_info("Encryption triggered successfully\n");
-	}
-
-out:
-    printk("VENCRYPT: %s.%d\n", __func__, __LINE__);
-	if (req)
-		skcipher_request_free(req);
-	return ret;
-}
-#endif
-
-static void decrypt(char __user *buffer, size_t size, loff_t *offset)
-{
+	return output_size;
 }
 
 /**
@@ -317,9 +177,46 @@ static ssize_t vencrypt_read(struct file *fp, char __user *user_buffer,
 {
 	vencrypt_device_data_t *vencrypt_data =
 		(vencrypt_device_data_t *)fp->private_data;
+	size_t copy_size = min(vencrypt_data->output_size, size);
+	size_t off = (offset == NULL) ? 0 : *offset;
 	printk("VENCRYPT: %s\n", __func__);
 
-	return 0;
+	if (copy_size == 0) {
+		printk("VENCRYPT: %s no data (%lu -> %lu)\n", __func__,
+		       vencrypt_data->output_size, size);
+		// no data to return
+		return 0;
+	}
+
+	if (encrypt) {
+		if (vencrypt_data != vencrypt_data->ct) {
+			// can't read from pt when encrypting
+			pr_info("VENCRYPT: %s can't read from pt when encrypting\n",
+				__func__);
+			return -EACCES;
+		}
+	} else {
+		if (vencrypt_data != vencrypt_data->pt) {
+			// can't read from ct when decrypting
+			pr_info("VENCRYPT: %s can't read from ct when decrypting\n",
+				__func__);
+			return -EACCES;
+		}
+	}
+
+	printk("VENCRYPT: %s copy %lu bytes to user\n", __func__, copy_size);
+	copy_to_user(user_buffer + off, vencrypt_data->output_data, copy_size);
+
+	vencrypt_data->output_offset += copy_size;
+	vencrypt_data->output_size -= copy_size;
+
+	// this is naive, need to use scatter gather?
+	if (vencrypt_data->output_size <= 0) {
+		vencrypt_data->output_size = 0;
+		vfree(vencrypt_data->output_data);
+	}
+
+	return copy_size;
 }
 
 /**
@@ -328,23 +225,32 @@ static ssize_t vencrypt_read(struct file *fp, char __user *user_buffer,
 static ssize_t vencrypt_write(struct file *fp, const char *buffer, size_t size,
 			      loff_t *offset)
 {
+	size_t output_size;
 	vencrypt_device_data_t *vencrypt_data =
 		(vencrypt_device_data_t *)fp->private_data;
 	printk("VENCRYPT: %s.%d fp %p data %p\n", __func__, __LINE__, fp,
 	       vencrypt_data);
 
-	vencrypt_data->output_size = 2048;
-	vencrypt_data->output_data =
-		kmalloc(vencrypt_data->output_size, GFP_KERNEL);
-
-	if (encrypt /* && dev == pt */) {
-		do_crypt(vencrypt_data, encrypt, key, buffer,
-			 size /*, offset */);
+	if (encrypt) {
+		if (vencrypt_data != vencrypt_data->pt) {
+			// can't write to ct when encrypting
+			pr_info("VENCRYPT: %s can't write to ct when encrypting\n",
+				__func__);
+			return -EACCES;
+		}
+	} else {
+		if (vencrypt_data != vencrypt_data->ct) {
+			// can't write to pt when decrypting
+			pr_info("VENCRYPT: %s can't write to pt when decrypting\n",
+				__func__);
+			return -EACCES;
+		}
 	}
 
-	kfree(vencrypt_data->output_data);
+	size_t off = (offset != NULL) ? *offset : 0;
+	output_size = do_crypt(vencrypt_data, encrypt, key, buffer, size, off);
 
-	return 0;
+	return size; // consumed bytes
 }
 
 static int vencrypt_release(struct inode *node, struct file *fp)
@@ -352,6 +258,8 @@ static int vencrypt_release(struct inode *node, struct file *fp)
 	vencrypt_device_data_t *vencrypt_data =
 		(vencrypt_device_data_t *)fp->private_data;
 	printk("VENCRYPT: %s\n", __func__);
+
+	// XXX free buffers
 
 	return 0;
 }
@@ -369,7 +277,7 @@ int vencrypt_init(void)
 {
 	int ind, retval, major;
 	dev_t dev;
-	int minor; // the minor device with the skcipher active
+	int minor; // the minor device with aes active
 
 	// set up cipher for CT node
 	if (encrypt) {
@@ -379,26 +287,23 @@ int vencrypt_init(void)
 		minor = nodes[VE_MINOR_CT]
 				.minor; // cyphertext node will be decrypting
 	}
-	devs[minor].skcipher = crypto_alloc_skcipher("cbc-aes-neonbs", 0, 0);
-	if (IS_ERR(devs[minor].skcipher)) {
-		pr_info("could not allocate skcipher handle: error %ld\n",
-			PTR_ERR(devs[minor].skcipher));
-		return PTR_ERR(devs[minor].skcipher);
-	}
 	printk("VENCRYPT: %s encrypt=%u key=%s\n", __func__, encrypt, key);
 	retval = alloc_chrdev_region(&device_number, 0, VE_MAX_DEVICES,
 				     "vencrypt");
 	if (retval) {
 		pr_err("%s: Failed to allocate device number error:%d\n",
 		       __func__, retval);
-		if (devs[minor].skcipher) {
-			crypto_free_skcipher(devs[minor].skcipher);
-		}
 		return retval;
 	}
 
 	major = MAJOR(device_number);
 	vencrypt_class = class_create(THIS_MODULE, "vencrypt_class");
+
+	// add reference to each node
+	devs[nodes[VE_MINOR_CT].minor].ct = &devs[nodes[VE_MINOR_CT].minor];
+	devs[nodes[VE_MINOR_CT].minor].pt = &devs[nodes[VE_MINOR_PT].minor];
+	devs[nodes[VE_MINOR_PT].minor].ct = &devs[nodes[VE_MINOR_CT].minor];
+	devs[nodes[VE_MINOR_PT].minor].pt = &devs[nodes[VE_MINOR_PT].minor];
 
 	// set up the device nodes
 	retval = 0;
@@ -420,10 +325,6 @@ int vencrypt_init(void)
 		}
 	}
 
-	if (retval && devs[minor].skcipher) {
-		crypto_free_skcipher(devs[minor].skcipher);
-	}
-
 	return retval;
 }
 
@@ -438,9 +339,6 @@ void vencrypt_exit(void)
 		/* release devs[ind] fields */
 		cdev_del(&devs[nodes[ind].minor].cdev);
 		device_destroy(vencrypt_class, dev);
-	}
-	if (devs[nodes[VE_MINOR_CT].minor].skcipher) {
-		crypto_free_skcipher(devs[nodes[VE_MINOR_CT].minor].skcipher);
 	}
 	class_destroy(vencrypt_class);
 	unregister_chrdev_region(device_number, VE_MAX_DEVICES);
